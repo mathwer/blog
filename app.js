789a541bc9c424c7a1b17ffd9ee5305326345535
MathWer <mat-wer@hotmail.com>
2020-10-15 22:58:30 -0300

    Blog agora com database funcional... Acredito eu

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash'); //Usando o lodash para facilitar encontrar a url, caso não exista
+const mongoose = require('mongoose');
+const config = require(__dirname + '/config.js') //Localização do database
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Bem vindo ao meu blog. Ele funciona de forma incremental, ou seja, cada post vai ser adicionado abaixo do post anterior. De forma que você poderá visualizar esse blog em ordem cronológica";
+const aboutContent = "Esse blog foi feito ao longo do curso 'The Complete 2020 Web Development Bootcamp' na Udemy. A princípio foi criado para treinar ejs e depois mongodb.";
+const contactContent = "Esse blog pertence a Matheus Werneck. Para contato, envie um email para matwer12@gmail.com. Obrigado pela visita!";
 
 const app = express();
 
@@ -18,11 +20,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+// -------------- Database --------------------
+
+mongoose.connect(config.mongoDB + '/blog', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = {
+  titulo: String,
+  texto: String
+}
+
+const Post = mongoose.model('Post', postSchema);
+
+ // -------------------------------------
 app.get('/', function(req, res){
-  
-  res.render('home', {
-    paraHome: homeStartingContent,
-    posts: posts
+   
+  Post.find(function(err, posts){
+    if(posts.length === 0){
+      console.log('Sem itens')
+      res.render('home', {paraHome: homeStartingContent})
+    } else{
+      res.render('home', {paraHome: homeStartingContent, posts: posts})
+    }
   })
 })
 
@@ -44,29 +62,39 @@ app.get('/compose', function(req, res){
 
 
 app.post('/compose', function (req, res){
-  const post = {titulo: req.body.titulo, texto:req.body.texto}
-  posts.push(post)
-  res.redirect('/')
-})
-
-
-
-app.get('/posts/:nomeDoPost', function(req, res){
-  const nome = _.lowerCase(req.params.nomeDoPost)
-  posts.forEach(function(item){
-    let tituloPost = _.lowerCase(item.titulo)
-    if(nome === tituloPost){
-      console.log('Correspondência encontrada')
-      res.render('post', {titulo: item.titulo, texto: item.texto})   
-    }
-    else{
-      console.log('Not found')
-    }
+  const post = new Post({titulo: req.body.titulo, texto:req.body.texto})
+  post.save(function(err){
+    res.redirect('/')
   })
 })
 
 
 
+// app.get('/posts/:nomeDoPost', function(req, res){
+//   const nome = _.lowerCase(req.params.nomeDoPost)
+//   posts.forEach(function(item){
+//     let tituloPost = _.lowerCase(item.titulo)
+//     if(nome === tituloPost){
+//       console.log('Correspondência encontrada')
+//       res.render('post', {titulo: item.titulo, texto: item.texto})   
+//     }
+//     else{
+//       console.log('Not found')
+//     }
+//   })
+// })
+
+app.get('/posts/:id', function(req, res){
+  const id = req.params.id
+  Post.findOne({_id: id}, function(err, post){
+    if (!post){
+      res.render('404')
+    }
+    else{
+      res.render('post', {titulo: post.titulo, texto: post.texto})
+    }
+  })
+})
 
 
 app.listen(3000, function() {
